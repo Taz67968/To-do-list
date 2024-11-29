d28b3d96202cb646ae218c83c8a7e943ed33108f
Tazoh Cliff <tazohcliff@gmail.com>
2024-11-29 15:45:38 +0100

    my work

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const submitButton = document.getElementById('submit-btn')
+const listInput = document.getElementById('guess')
+const tasklist = document.getElementById('list')
+
+submitButton.addEventListener('click', () => {
+  const uliterm = document.createElement('li')
+  uliterm.innerHTML = listInput.value
+  tasklist.append(uliterm)
+  listInput.value = ''
+})
